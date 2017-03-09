675a8692f4d2cd49d897cb6014a1f2d8027dd113
Katie Koschland <katiekoschland@hotmail.com>
2017-03-09 14:03:16 +0000

    adding tags

--- app/app.rb
+++ app/app.rb
@@ -7,8 +7,8 @@ require_relative 'data_mapper_setup'
 class BookmarkManager < Sinatra::Base
 
   get '/' do
-   "Hello World"
- end
+    "Hello World"
+  end
 
   get '/links' do
     @links = Link.all
@@ -19,15 +19,21 @@ class BookmarkManager < Sinatra::Base
     erb :'links/new'
   end
 
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+
   post '/links' do
     link = Link.new(url: params[:url], title:params[:title])
     params[:tags].split.each do |tag|
       link.tags << Tag.first_or_create(name: tag)
+    end
+    link.save
+    redirect '/links'
   end
-  link.save
-  redirect '/links'
-end
 
-run! if app_file == $0
+  run! if app_file == $0
 
 end
--- spec/viewing_links_spec.rb
+++ spec/viewing_links_spec.rb
@@ -12,4 +12,26 @@ feature 'Viewing links' do
       expect(page).to have_content('Google')
     end
   end
+
+
+  before(:each) do
+    Link.create(url: 'https://makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'https://google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
+    Link.create(url: 'https://zombo.com', title: 'This is Zombocom', tags: [Tag.first_or_create(name: 'bubbles')])
+    Link.create(url: 'https://bubble-bobble.com', title: 'Bubble Bobble', tags: [Tag.first_or_create(name: 'bubbles')])
+  end
+
+  scenario 'I can filter links by tag' do
+    visit '/tags/bubbles'
+
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).not_to have_content('Makers Academy')
+      expect(page).not_to have_content('Code.org')
+      expect(page).to have_content('This is Zombocom')
+      expect(page).to have_content('Bubble Bobble')
+    end
+  end
+
 end
